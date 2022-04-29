566f3c1f9f1b07a4c441a50122fc1e8ae1c17c6d
Xelemental69 <xelemental864@gmail.com>
2022-04-29 13:24:40 +0200

    POR TERMINAR

---
+++ tarea4-GUI-FJMG/src/main/java/calculadora/Calculadora.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author fcoj
+ */
+import javax.swing.*;
+import paneles.*;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Calculadora");
+        frame.setBounds(250, 250, 0, 0);
+        frame.add(new MainPanel());
+
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+
+    }
+
+}
